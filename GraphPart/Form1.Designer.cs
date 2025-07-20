179ebfa26ded78b3c2131288bb5ef54231eac43d
Risen <Risen@DESKTOP-4FP6MS9>
2025-07-20 12:51:05 +0300

    Добавьте файлы проекта.

---
+++ GraphPart/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿using System.Windows.Forms;
+
+namespace GraphPart
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip = new MenuStrip();
+            fileMenu = new ToolStripMenuItem();
+            newMenuItem = new ToolStripMenuItem();
+            saveMenuItem = new ToolStripMenuItem();
+            loadMenuItem = new ToolStripMenuItem();
+            assembleMenuItem = new ToolStripMenuItem();
+            runMenuItem = new ToolStripMenuItem();
+            stepMenuItem = new ToolStripMenuItem();
+            resetMenuItem = new ToolStripMenuItem();
+            this.helpMenuItem = new ToolStripMenuItem("Справка");
+            dgvRegisters = new DataGridView();
+            dataGridViewTextBoxColumn5 = new DataGridViewTextBoxColumn();
+            dataGridViewTextBoxColumn6 = new DataGridViewTextBoxColumn();
+            dgvMemory = new DataGridView();
+            dataGridViewTextBoxColumn1 = new DataGridViewTextBoxColumn();
+            dataGridViewTextBoxColumn2 = new DataGridViewTextBoxColumn();
+            dataGridViewTextBoxColumn3 = new DataGridViewTextBoxColumn();
+            dataGridViewTextBoxColumn4 = new DataGridViewTextBoxColumn();
+            txtProgram = new RichTextBox();
+            splitContainer = new SplitContainer();
+            dropdownSelector = new ComboBox();
+            readOnlyTextBox1 = new TextBox();
+            readOnlyTextBox2 = new TextBox();
+            editorTopPanel = new Panel();
+            menuStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvRegisters).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dgvMemory).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)splitContainer).BeginInit();
+            splitContainer.Panel1.SuspendLayout();
+            splitContainer.Panel2.SuspendLayout();
+            splitContainer.SuspendLayout();
+            editorTopPanel.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            menuStrip.Items.AddRange(new ToolStripItem[] { fileMenu, assembleMenuItem, runMenuItem, stepMenuItem, resetMenuItem, helpMenuItem});
+            menuStrip.Location = new Point(0, 0);
+            menuStrip.Name = "menuStrip";
+            menuStrip.Size = new Size(784, 24);
+            menuStrip.TabIndex = 0;
+            menuStrip.Text = "menuStrip";
+            // 
+            // fileMenu
+            // 
+            fileMenu.DropDownItems.AddRange(new ToolStripItem[] { newMenuItem, saveMenuItem, loadMenuItem });
+            fileMenu.Name = "fileMenu";
+            fileMenu.Size = new Size(48, 20);
+            fileMenu.Text = "Файл";
+            // 
+            // newMenuItem
+            // 
+            newMenuItem.Name = "newMenuItem";
+            newMenuItem.Size = new Size(133, 22);
+            newMenuItem.Text = "Новый";
+            newMenuItem.Click += newMenuItem_Click;
+            // 
+            // saveMenuItem
+            // 
+            saveMenuItem.Name = "saveMenuItem";
+            saveMenuItem.Size = new Size(133, 22);
+            saveMenuItem.Text = "Сохранить";
+            saveMenuItem.Click += saveMenuItem_Click;
+            // 
+            // loadMenuItem
+            // 
+            loadMenuItem.Name = "loadMenuItem";
+            loadMenuItem.Size = new Size(133, 22);
+            loadMenuItem.Text = "Загрузить";
+            loadMenuItem.Click += loadMenuItem_Click;
+            // 
+            // assembleMenuItem
+            // 
+            assembleMenuItem.Name = "assembleMenuItem";
+            assembleMenuItem.Size = new Size(114, 20);
+            assembleMenuItem.Text = "Скомпилировать";
+            assembleMenuItem.Click += assembleMenuItem_Click;
+            // 
+            // runMenuItem
+            // 
+            runMenuItem.Name = "runMenuItem";
+            runMenuItem.Size = new Size(74, 20);
+            runMenuItem.Text = "Запустить";
+            runMenuItem.Click += runMenuItem_Click;
+            // 
+            // stepMenuItem
+            // 
+            stepMenuItem.Name = "stepMenuItem";
+            stepMenuItem.Size = new Size(41, 20);
+            stepMenuItem.Text = "Шаг";
+            stepMenuItem.Click += stepMenuItem_Click;
+            // 
+            // resetMenuItem
+            // 
+            resetMenuItem.Name = "resetMenuItem";
+            resetMenuItem.Size = new Size(54, 20);
+            resetMenuItem.Text = "Сброс";
+            resetMenuItem.Click += resetMenuItem_Click;
+
+            helpMenuItem.Click += ShowHelp;
+            menuStrip.Items.Add(helpMenuItem);
+            // 
+            // dgvRegisters
+            // 
+            dgvRegisters.AllowUserToAddRows = false;
+            dgvRegisters.AllowUserToDeleteRows = false;
+            dgvRegisters.BackgroundColor = SystemColors.Control;
+            dgvRegisters.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvRegisters.Columns.AddRange(new DataGridViewColumn[] { dataGridViewTextBoxColumn5, dataGridViewTextBoxColumn6 });
+            dgvRegisters.Dock = DockStyle.Top;
+            dgvRegisters.Location = new Point(0, 0);
+            dgvRegisters.Name = "dgvRegisters";
+            dgvRegisters.ReadOnly = true;
+            dgvRegisters.Size = new Size(305, 126);
+            dgvRegisters.TabIndex = 1;
+            
+            // 
+            // dataGridViewTextBoxColumn5
+            // 
+            dataGridViewTextBoxColumn5.HeaderText = "Регистр/Флаг";
+            dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
+            dataGridViewTextBoxColumn5.ReadOnly = true;
+            dataGridViewTextBoxColumn5.Width = 162;
+            // 
+            // dataGridViewTextBoxColumn6
+            // 
+            dataGridViewTextBoxColumn6.HeaderText = "Значение";
+            dataGridViewTextBoxColumn6.Name = "dataGridViewTextBoxColumn6";
+            dataGridViewTextBoxColumn6.ReadOnly = true;
+
+            dgvRegisters.Rows.Add("Память[A1]", 0);
+            dgvRegisters.Rows.Add("Память[A2]", 0);
+            dgvRegisters.Rows.Add("Память[A3]", 0);
+            dgvRegisters.Rows.Add("Флаг переполнения", "000");
+            // 
+            // dgvMemory
+            // 
+            dgvMemory.AllowUserToAddRows = false;
+            dgvMemory.AllowUserToDeleteRows = false;
+            dgvMemory.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            dgvMemory.BackgroundColor = SystemColors.Control;
+            dgvMemory.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvMemory.Columns.AddRange(new DataGridViewColumn[] { dataGridViewTextBoxColumn1, dataGridViewTextBoxColumn2, dataGridViewTextBoxColumn3, dataGridViewTextBoxColumn4 });
+            dgvMemory.Dock = DockStyle.Fill;
+            dgvMemory.Location = new Point(0, 126);
+            dgvMemory.Name = "dgvMemory";
+            dgvMemory.ReadOnly = true;
+            dgvMemory.Size = new Size(305, 285);
+            dgvMemory.TabIndex = 0;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            dataGridViewTextBoxColumn1.HeaderText = "Адрес";
+            dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            dataGridViewTextBoxColumn1.ReadOnly = true;
+            dataGridViewTextBoxColumn1.Width = 65;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            dataGridViewTextBoxColumn2.HeaderText = "PC";
+            dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            dataGridViewTextBoxColumn2.ReadOnly = true;
+            dataGridViewTextBoxColumn2.Width = 47;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            dataGridViewTextBoxColumn3.HeaderText = "Код";
+            dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            dataGridViewTextBoxColumn3.ReadOnly = true;
+            dataGridViewTextBoxColumn3.Width = 52;
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            dataGridViewTextBoxColumn4.HeaderText = "Мнемоника";
+            dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            dataGridViewTextBoxColumn4.ReadOnly = true;
+            dataGridViewTextBoxColumn4.Width = 98;
+            // 
+            // txtProgram
+            // 
+            txtProgram.AcceptsTab = true;
+            txtProgram.Dock = DockStyle.Fill;
+            txtProgram.Font = new Font("Consolas", 10F);
+            txtProgram.Location = new Point(0, 0);
+            txtProgram.Name = "txtProgram";
+            txtProgram.Size = new Size(451, 411);
+            txtProgram.TabIndex = 0;
+            txtProgram.Text = "";
+            this.txtProgram.TextChanged += txtProgram_TextChanged;
+            // 
+            // splitContainer
+            // 
+            splitContainer.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            splitContainer.Location = new Point(12, 38);
+            splitContainer.Name = "splitContainer";
+            // 
+            // splitContainer.Panel1
+            // 
+            splitContainer.Panel1.Controls.Add(dgvMemory);
+            splitContainer.Panel1.Controls.Add(dgvRegisters);
+            // 
+            // splitContainer.Panel2
+            // 
+            this.splitContainer.Panel2.Controls.Add(this.txtProgram);
+            this.splitContainer.Panel2.Controls.Add(this.editorTopPanel);
+            splitContainer.Size = new Size(760, 411);
+            splitContainer.SplitterDistance = 305;
+            splitContainer.TabIndex = 0;
+            // 
+            // dropdownSelector
+            // 
+            dropdownSelector.DropDownStyle = ComboBoxStyle.DropDownList;
+            dropdownSelector.Items.AddRange(new object[] { "Компиляция", "Декомпиляция" });
+            dropdownSelector.Location = new Point(0, 0);
+            dropdownSelector.Name = "dropdownSelector";
+            dropdownSelector.Size = new Size(150, 23);
+            dropdownSelector.TabIndex = 0;
+            dropdownSelector.SelectedIndex = 0;
+            // 
+            // readOnlyTextBox1
+            // 
+            readOnlyTextBox1.Location = new Point(160, 0);
+            readOnlyTextBox1.Name = "readOnlyTextBox1";
+            readOnlyTextBox1.ReadOnly = true;
+            readOnlyTextBox1.Size = new Size(290, 23);
+            readOnlyTextBox1.TabIndex = 1;
+            readOnlyTextBox1.Text = "Для запуска программы проведите компиляцию!";
+            // 
+            // readOnlyTextBox2
+            // 
+            readOnlyTextBox2.Location = new Point(0, 25);
+            readOnlyTextBox2.Name = "readOnlyTextBox2";
+            readOnlyTextBox2.ReadOnly = true;
+            readOnlyTextBox2.Size = new Size(450, 23);
+            readOnlyTextBox2.TabIndex = 2;
+            readOnlyTextBox2.Text = "Ошибка компиляции: Отсутствует.";
+            // 
+            // editorTopPanel
+            // 
+            editorTopPanel.Controls.Add(dropdownSelector);
+            editorTopPanel.Controls.Add(readOnlyTextBox1);
+            editorTopPanel.Controls.Add(readOnlyTextBox2);
+            editorTopPanel.Dock = DockStyle.Top;
+            editorTopPanel.Location = new Point(0, 0);
+            editorTopPanel.Name = "editorTopPanel";
+            editorTopPanel.Size = new Size(200, 50);
+            editorTopPanel.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            ClientSize = new Size(784, 461);
+            Controls.Add(splitContainer);
+            Controls.Add(menuStrip);
+            MainMenuStrip = menuStrip;
+            Name = "Form1";
+            Text = "Лучше чем КР580";
+            menuStrip.ResumeLayout(false);
+            menuStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvRegisters).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dgvMemory).EndInit();
+            splitContainer.Panel1.ResumeLayout(false);
+            splitContainer.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer).EndInit();
+            splitContainer.ResumeLayout(false);
+            editorTopPanel.ResumeLayout(false);
+            editorTopPanel.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn6;
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+    }
+}
---
+++ GraphPart/Form1.cs
@@ -0,0 +1,322 @@
+using System.Collections;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GraphPart
+{
+    public partial class Form1 : Form
+    {
+        private MenuStrip menuStrip;
+        private ToolStripMenuItem fileMenu;
+        private ToolStripMenuItem newMenuItem;
+        private ToolStripMenuItem loadMenuItem;
+        private ToolStripMenuItem saveMenuItem;
+        private ToolStripMenuItem assembleMenuItem;
+        private ToolStripMenuItem runMenuItem;
+        private ToolStripMenuItem stepMenuItem;
+        private ToolStripMenuItem resetMenuItem;
+        private ToolStripMenuItem helpMenuItem;
+
+        private DataGridView dgvRegisters;
+        private DataGridView dgvMemory;
+        private RichTextBox txtProgram;
+        private SplitContainer splitContainer;
+        private ComboBox dropdownSelector;
+        private TextBox readOnlyTextBox1;
+        private TextBox readOnlyTextBox2;
+        private Panel editorTopPanel;
+        private string[] parts;
+        private byte[] decData;
+        private string made;
+        private int currentIndex = 1;
+        private int curr = 0;
+        private bool state = false;
+        private int[] regs = new int[5];
+        private bool end = true;
+        private bool compiled = false;
+        private int byteNow = 1;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void newMenuItem_Click(object sender, EventArgs e) => txtProgram.Clear();
+        private void loadMenuItem_Click(object sender, EventArgs e)
+        {
+            using (var dlg = new OpenFileDialog())
+            {
+                dlg.Filter = "Файлы ассемблера|*.asm;*.txt|Все файлы|*.*";
+                if (dlg.ShowDialog() == DialogResult.OK)
+                    txtProgram.Text = File.ReadAllText(dlg.FileName);
+            }
+        }
+        private void saveMenuItem_Click(object sender, EventArgs e)
+        {
+            using (var dlg = new SaveFileDialog())
+            {
+                dlg.Filter = "Файлы ассемблера|*.asm;*.txt|Все файлы|*.*";
+                if (dlg.ShowDialog() == DialogResult.OK)
+                    File.WriteAllText(dlg.FileName, txtProgram.Text);
+            }
+        }
+
+        // Action menu handlers
+        private void assembleMenuItem_Click(object sender, EventArgs e)
+        {
+            string inputText;
+            if (dropdownSelector.SelectedIndex == 0)
+                inputText = "1\n" + txtProgram.Text;
+            else
+                inputText = "0\n" + txtProgram.Text;
+            string result = RunProgram1("AssemblyCompilerMK4.exe", inputText);
+            if (result[0] == 'E')
+            {
+                Error(result);
+                return;
+            }
+            result = result.Substring(3, result.Length - 4);
+            if (dropdownSelector.SelectedIndex == 0)
+                Assemble(result);
+            else
+                Disassemble(result);
+        }
+        private void Assemble(string s)
+        {
+            decData = ConvertToByteCode(s);
+            File.WriteAllBytes("compiled.bin", decData);
+            readOnlyTextBox2.Text = "Программа скомпилирована без ошибок";
+            compiled = true;
+            readOnlyTextBox1.BackColor = Color.White;
+            readOnlyTextBox1.Text = "Программа готова к запуску";
+            made = RunProgram1("AssemblyCompilerMK4.exe", "0\n" + s);
+            GetReadyPC(s);
+        }
+        private void Disassemble(string s)
+        {
+            readOnlyTextBox2.Text = "Программа декомпилирована без ошибок";
+            txtProgram.Text = s;
+        }
+        private void GetReadyPC(string s)
+        {
+            if (dgvMemory.Rows.Count > 0)
+            {
+                dgvMemory.Rows.Clear();
+                byteNow = 1;
+            }
+                
+            string[] tempStr = s.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            string[] tempStr2 = made.Substring(3, made.Length - 4).Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0, j = 0; j < tempStr2.Length; j++, i++)
+            {
+                switch (tempStr2[j].Substring(0, 4))
+                {
+                    case "MOV ":
+                        dgvMemory.Rows.Add(byteNow, "", tempStr[i] + tempStr[i+1], tempStr2[j]);
+                        i++;
+                        byteNow += 2;
+                        break;
+                    default:
+                        dgvMemory.Rows.Add(byteNow, "", tempStr[i], tempStr2[j]);
+                        byteNow++;
+                        break;
+                }
+
+            }
+        }
+        private void Error(string s)
+        {
+            readOnlyTextBox2.Text = "Ошибка компиляции: " + s;
+        }
+        private async void runMenuItem_Click(object sender, EventArgs e)
+        {
+            if (compiled)
+            {
+                runMenuItem.Text = runMenuItem.Text == "Запустить" ? "Остановить" : "Запустить";
+                if (state)
+                    state = false;
+                else
+                {
+                    if (end)
+                    {
+                        newStart();
+                    }
+                    state = true;
+                    for (int i = currentIndex; !state || i < parts.Length; i++)
+                    {
+                        await Task.Delay(100);
+                        Start(parts[i]);
+                        end = false;
+                        if (i == parts.Length - 1 || state == false)
+                            break;
+                    }
+                    runMenuItem.Text = "Запустить";
+                    if (currentIndex == parts.Length-1)
+                    {
+                        currentIndex = 1;
+                        curr = 0;
+                        end = true;
+                        state = false;
+                    }
+                }
+            }
+            else
+            {
+                readOnlyTextBox1.BackColor = Color.Red;
+            }
+        }
+        private void Start(string partsi)
+        {
+            string[] tempStr = partsi.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            for (int j = 0; j < 5; j++)
+                regs[j] = Int32.Parse(tempStr[j]);
+            ChangeRegs(regs);
+            if (curr != 0)
+                dgvMemory.Rows[curr - 1].Cells[1].Value = "";
+            if (curr < dgvMemory.Rows.Count)
+            {
+                dgvMemory.Rows[curr].Cells[1].Value = ">>>>";
+                curr++;
+            }
+            else
+            {
+                currentIndex = 1;
+                curr = 0;
+                end = true;
+                state = false;
+            }
+            dgvRegisters.Refresh();
+            currentIndex++;
+        }
+        private void ChangeRegs(int[] r)
+        {
+            dgvRegisters.Rows[0].Cells[1].Value = r[1];
+            dgvRegisters.Rows[1].Cells[1].Value = r[2];
+            dgvRegisters.Rows[2].Cells[1].Value = r[3];
+            dgvRegisters.Rows[3].Cells[1].Value = r[4];
+        }
+        private void stepMenuItem_Click(object sender, EventArgs e)
+        {
+            if (parts == null || currentIndex == parts.Length-1) return;
+            Start(parts[currentIndex]);
+        }
+        private void resetMenuItem_Click(object sender, EventArgs e)
+        {
+            end = true;
+            state = false;
+            currentIndex = 1;
+            dgvMemory.Rows[curr-1].Cells[1].Value = "";
+            curr = 0;
+            ChangeRegs([0,0,0,0,0]);
+            newStart();
+        }
+        private void txtProgram_TextChanged(object sender, EventArgs e)
+        {
+            compiled = false;
+            readOnlyTextBox1.Text = "Для запуска программы проведите компиляцию!";
+        }
+        private void newStart()
+        {
+            string result = RunProgram2("Emulator.exe");
+            parts = result.Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+        private string RunProgram1(string executablePath, string inputText)
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = executablePath;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardInput = true;
+            process.StartInfo.CreateNoWindow = true;
+
+            process.Start();
+            process.StandardInput.Write(inputText);
+            process.StandardInput.Close();
+
+            string output = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+            return output;
+        }
+        private string RunProgram2(string executablePath)
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = executablePath;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardInput = true;
+            process.StartInfo.CreateNoWindow = true;
+
+            process.Start();
+            process.StandardInput.Write(new StreamReader("code.bin").ReadToEnd());
+            process.StandardInput.Close();
+
+            string output = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+            return output;
+        }
+        public static byte[] ConvertToByteCode(string bitString)
+        {
+            string z = "00000000";
+            var bits = new string(bitString
+                .Where(c => c == '0' || c == '1')
+                .ToArray());
+
+            if (bits.Length % 8 != 0)
+                throw new Exception("Строка битов должна быть кратна 8");
+
+            int byteCount = bits.Length / 8;
+            byte[] result = new byte[2047];
+            int i;
+            for (i = 0; i < byteCount; i++)
+            {
+                string byteBits = bits.Substring(i * 8, 8);
+                result[i] = Convert.ToByte(byteBits, 2);
+            }
+            for (int j = i; j < 2047; j++)
+            {
+                result[i] = Convert.ToByte(z, 2);
+            }
+            
+            return result;
+        }
+        private void ShowHelp(object sender, EventArgs e)
+        {
+            string message =
+                "Код   Мнемокод       Описание\n" +
+                "00000 MOVA1 const   A1 = константа\n" +
+                "00001 MOVA2 const   A2 = константа\n" +
+                "00010 MOVA3 const   A3 = константа\n" +
+                "00100 ADD страница  Память[A3] = Память[A1] + Память[A2]\n" +
+                "00101 ADC страница  Память[A3] = Память[A1] + Память[A2] + флаг перен.\n" +
+                "00110 SUB страница  Память[A3] = Память[A1] - Память[A2]\n" +
+                "00111 SBB страница  Память[A3] = Память[A1] - Память[A2] - флаг перен.\n" +
+                "01000 MUL страница  Память[A3] = Память[A1] * Память[A2]\n" +
+                "01001 DIV страница  Память[A3] = Память[A1] / Память[A2]\n" +
+                "01010 MOD страница  Память[A3] = Память[A1] % Память[A2]\n" +
+                "01011 ABS страница  Память[A3] = |Память[A1]|\n" +
+                "01100 AND страница  Память[A3] = Память[A1] & Память[A2]\n" +
+                "01101 OR страница   Память[A3] = Память[A1] | Память[A2]\n" +
+                "01110 XOR страница  Память[A3] = Память[A1] ^ Память[A2]\n" +
+                "01111 NOT страница  Память[A3] = not Память[A1]\n" +
+                "10000 JMP страница  Переход по адресу A3\n" +
+                "10001 JB страница   Переход по адресу A3, если результат < 0\n" +
+                "10010 JNZ страница  Переход по адресу A3, если результат != 0\n" +
+                "10011 MOVS         Память[A3] = A1, A3 += 1, A1 += 1\n" +
+                "10101 CMPS         Сравнение Память[A1] и Память[A3]\n\n" +
+                "Флаг переполнения: представляет собой 3 бита (значение от 0 до 7),\n" +
+                "где первый бит означает переполнение, второй бит - результат меньше нуля, третий - не равен нулю.\n" +
+                "Флаг 4 ознчает 100, Флаг 1 = 001 и т.д.";
+
+
+            MessageBox.Show(message, "Справка", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        /*
+         * dgvRegisters.Rows.Add("Память[A1]", 0);
+            dgvRegisters.Rows.Add("Память[A2]", 0);
+            dgvRegisters.Rows.Add("Память[A3]", 0);
+            dgvRegisters.Rows.Add("Флаг переполнения", "000");
+         * */
+    }
+}
